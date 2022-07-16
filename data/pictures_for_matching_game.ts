991bc59e573daedab3898de3e5ccf894e498b1b4
DEVRafam <jodax95@gmail.com>
2022-07-16 15:38:25 +0200

    📦 NEW: Images for matching

---
+++ data/pictures_for_matching_game.ts
@@ -0,0 +1,47 @@
+export interface PictureToMatch {
+    title: string;
+    folder: string;
+}
+
+export default [
+    {
+        title: "Bathroom furniture interior design",
+        folder: "bathroom-furniture-interior-design",
+    },
+    {
+        title: "Beach beach resort antalya",
+        folder: "beach-beach-resort-antalya",
+    },
+    {
+        title: "Building queen sofia palace of arts",
+        folder: "building-queen-sofia-palace-of-arts",
+    },
+    {
+        title: "Cheetah africa safari cat wildlife",
+        folder: "cheetah-africa-safari-cat-wildlife",
+    },
+    {
+        title: "Chrysanthemum flowers plants nature",
+        folder: "chrysanthemum-flowers-plants-nature",
+    },
+    {
+        title: "Grain agriculture golden sunset",
+        folder: "grain-agriculture-golden-sunset",
+    },
+    {
+        title: "Mount kilimanjaro mountain tanzania",
+        folder: "mount-kilimanjaro-mountain-tanzania",
+    },
+    {
+        title: "Opium poppy papaver orientale",
+        folder: "opium-poppy-papaver-orientale",
+    },
+    {
+        title: "Sea lighthouse sunset water",
+        folder: "sea-lighthouse-sunset-water",
+    },
+    {
+        title: "Woman nature portrait outdoors",
+        folder: "woman-nature-portrait-outdoors",
+    },
+] as PictureToMatch[];
