7791e78adbd1a1b145f7e993b907236209514881
DEVRafam <jodax95@gmail.com>
2022-07-22 18:47:40 +0200

    📦 NEW: Prisma configuration
    See #9

---
+++ @types/Project.ts
@@ -0,0 +1,10 @@
+export interface Description {
+    introduction: string;
+    purpose: string;
+    conclusion: string;
+}
+
+export interface Feature {
+    title: string;
+    imageURL: string;
+}
