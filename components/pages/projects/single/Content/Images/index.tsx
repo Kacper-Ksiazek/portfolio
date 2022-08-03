adc01da0d265aca6b85256ed8279f16b9b6644d1
DEVRafam <jodax95@gmail.com>
2022-08-03 04:34:36 +0200

    📦 NEW: Single project / Content
    See #29

--- components/pages/projects/single/Content/Images/index.tsx
+++ components/pages/projects/single/Content/Images/index.tsx
@@ -1,5 +1,4 @@
 // Tools
-import { styled } from "@mui/system";
 import { useState, useEffect, useRef } from "react";
 import useImagesWrapperContext from "@/components/pages/projects/single/Content/Images/hooks/useImagesWrapperContext";
 // Types
@@ -11,18 +10,7 @@ import ProjectThumbnail from "./ProjectThumbnail";
 import { ImagesWrapperContextProvider } from "./context";
 import BottomRightCornerActions from "./BottomRightCornerActions";
 // Styled components
-const PictureWrapper = styled("div")(({ theme }) => ({
-    width: "100%",
-    position: "relative",
-    borderRadius: "10px",
-    overflow: "hidden",
-    margin: "20px 0",
-    //
-    height: "550px",
-    "#features-overflow-hidden-container": {
-        height: "467px",
-    },
-}));
+import PictureWrapper from "./MainWrapper";
 
 interface ContextWrapperProps {
     folder: string;
@@ -34,15 +22,12 @@ const ImagesWrapper: FunctionComponent = () => {
 
     const pendingTimeout = useRef<number>(-1);
     const [renderFeatures, setRenderFeatures] = useState<boolean>(false);
-    const [renderProjectThumbnail, setRenderProjectThumbnail] = useState<boolean>(true);
 
     useEffect(() => {
         clearTimeout(pendingTimeout.current);
         if (context.openBrowseFeatures === true) {
             setRenderFeatures(true);
-            pendingTimeout.current = setTimeout(() => setRenderProjectThumbnail(false), 1000) as any;
         } else if (context.openBrowseFeatures === false) {
-            setRenderProjectThumbnail(true);
             pendingTimeout.current = setTimeout(() => setRenderFeatures(false), 1000) as any;
         }
     }, [context.openBrowseFeatures]);
@@ -51,7 +36,7 @@ const ImagesWrapper: FunctionComponent = () => {
         <PictureWrapper id="images-wrapper">
             <BottomRightCornerActions />
 
-            {renderProjectThumbnail && <ProjectThumbnail />}
+            <ProjectThumbnail />
             {renderFeatures && <Features />}
         </PictureWrapper>
     );
--- components/pages/projects/single/Content/index.tsx
+++ components/pages/projects/single/Content/index.tsx
@@ -1,15 +1,56 @@
 // Tools
+import { styled } from "@mui/system";
+import fadeSimple from "@/components/_keyframes/intro/fadeSimple";
+import fadeFromLeft from "@/components/_keyframes/intro/fadeFromLeft";
+import fadeFromBottom from "@/components/_keyframes/intro/fadeFromBottom";
 import formatTextViaBolding from "@/utils/client/formatTextViaBolding";
 // Types
 import type { FunctionComponent } from "react";
 import type { Project } from "@/@types/pages/projects/SingleProject";
 // Other components
-import Duration from "../../../_shared/single-project/Duration";
 import Redirects from "./Redirects";
 import ImagesWrapper from "./Images";
 import { Paragraph, Header } from "./TextStyledComponents";
+import Duration from "../../../_shared/single-project/Duration";
+import VisibilitySensor from "@/components/_utils/VisibilitySensor";
 import DisplayTechnologies from "@/components/_utils/DisplayTechnologies";
 import LightSectionWrapper from "@/components/_styled_components/content_placement/SectionWrapper/Light";
+// Styled components
+const DescriptionWrapper = styled("div")(({ theme }) => ({
+    "p, h3, a": {
+        visibility: "hidden",
+    },
+    "&.visible": {
+        "p, h3, a": {
+            visibility: "visible",
+        },
+        h3: {
+            "&:nth-of-type(1)": {
+                animation: `${fadeFromLeft} .3s .2s both linear`,
+            },
+            "&:nth-of-type(2)": {
+                animation: `${fadeFromLeft} .3s .3s both linear`,
+            },
+            "&:nth-of-type(3)": {
+                animation: `${fadeFromLeft} .3s .4s both linear`,
+            },
+        },
+        p: {
+            "&:nth-of-type(1)": {
+                animation: `${fadeFromLeft} .3s .25s both linear`,
+            },
+            "&:nth-of-type(2)": {
+                animation: `${fadeFromLeft} .3s .35s both linear`,
+            },
+            "&:nth-of-type(3)": {
+                animation: `${fadeFromLeft} .3s .45s both linear`,
+            },
+        },
+        a: {
+            animation: `${fadeFromBottom} .3s 1s both linear`,
+        },
+    },
+}));
 
 interface SingleProjectContentProps {
     project: Project;
@@ -23,28 +64,37 @@ const SingleProjectContent: FunctionComponent<SingleProjectContentProps> = ({ pr
                 main: project.title,
                 additionalJSX: (
                     <>
-                        <Duration start={project.start} end={project.end} />
-                        <DisplayTechnologies technologies={project.technologies} />
+                        <Duration
+                            start={project.start} //
+                            end={project.end}
+                            sx={{ animation: `${fadeSimple} .2s .6s both linear` }}
+                        />
+                        <DisplayTechnologies technologies={project.technologies} firstAnimationDelay={0.8} />
                     </>
                 ),
+                estimatedHeight: "180px",
             }}
             round="left"
             unlimitedHeight
         >
-            <Paragraph>{formatTextViaBolding(project.shortDescription, true)}</Paragraph>
+            <Paragraph sx={{ animation: `${fadeFromLeft} .3s 1s both linear` }}>{formatTextViaBolding(project.shortDescription, true)}</Paragraph>
 
             <ImagesWrapper features={project.features} folder={project.folder} />
 
-            <Header>Introduction and quick overview</Header>
-            <Paragraph>{formatTextViaBolding(project.description.introduction, true)}</Paragraph>
+            <VisibilitySensor>
+                <DescriptionWrapper>
+                    <Header>Introduction and quick overview</Header>
+                    <Paragraph>{formatTextViaBolding(project.description.introduction, true)}</Paragraph>
 
-            <Header>The purpose of the application</Header>
-            <Paragraph>{formatTextViaBolding(project.description.purpose, true)}</Paragraph>
+                    <Header>The purpose of the application</Header>
+                    <Paragraph>{formatTextViaBolding(project.description.purpose, true)}</Paragraph>
 
-            <Header>Conclusion and finals thoughts</Header>
-            <Paragraph>{formatTextViaBolding(project.description.conclusion, true)}</Paragraph>
+                    <Header>Conclusion and finals thoughts</Header>
+                    <Paragraph>{formatTextViaBolding(project.description.conclusion, true)}</Paragraph>
 
-            <Redirects githubURL={project.githubURL} liveDemoURL={project.liveDemoURL} />
+                    <Redirects githubURL={project.githubURL} liveDemoURL={project.liveDemoURL} />
+                </DescriptionWrapper>
+            </VisibilitySensor>
         </LightSectionWrapper>
     );
 };
