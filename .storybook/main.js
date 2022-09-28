f02545a5b7336cafd516f0377a6ad0d4ab5daf85
DEVRafam <jodax95@gmail.com>
2022-09-28 02:19:37 +0200

    👌 IMPROVE: Storybook setup file

--- .storybook/main.js
+++ .storybook/main.js
@@ -19,13 +19,6 @@ const config = {
     webpackFinal: async (config) => {
         config.resolve.plugins = [new TsconfigPathsPlugin({ extensions: config.resolve.extensions })];
 
-        // return merge(config, {
-        //     resolve: {
-        //         alias: {
-        //             "@emotion/react": getPackageDir("@emotion/react"),
-        //         },
-        //     },
-        // });
         return config;
     },
     core: {
@@ -34,17 +27,3 @@ const config = {
 };
 
 module.exports = config;
-
-function getPackageDir(filepath) {
-    let currDir = path.dirname(require.resolve(filepath));
-    while (true) {
-        if (fs.existsSync(path.join(currDir, "package.json"))) {
-            return currDir;
-        }
-        const { dir, root } = path.parse(currDir);
-        if (dir === root) {
-            throw new Error(`Could not find package.json in the parent directories starting from ${filepath}.`);
-        }
-        currDir = dir;
-    }
-}
