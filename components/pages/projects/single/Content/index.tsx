917e476c4a90aec2995e2305a3e2884c1cc7e830
DEVRafam <jodax95@gmail.com>
2022-10-01 11:51:08 +0200

    📦 NEW: Render FeaturesList only when there are some features
    See #11

--- components/pages/projects/single/Content/index.tsx
+++ components/pages/projects/single/Content/index.tsx
@@ -1,17 +1,21 @@
+// Tools
+import dynamic from "next/dynamic";
 // Types
 import type { FunctionComponent } from "react";
 import type { Project } from "@/@types/pages/projects/SingleProject";
 // Other components
 import ProjectDescription from "./ProjectDescription";
-import FeaturesList from "./FeaturesList";
-import LightSectionWrapper from "@/components/atoms/content_placement/SectionWrapper/Light";
+const FeaturesList = dynamic(() => import("./FeaturesList"));
 // Styled components
+import LightSectionWrapper from "@/components/atoms/content_placement/SectionWrapper/Light";
 
 interface SingleProjectContentProps {
     project: Project;
 }
 
 const SingleProjectContent: FunctionComponent<SingleProjectContentProps> = ({ project }) => {
+    const amountOfFeatures: number = project.features.length;
+
     return (
         <LightSectionWrapper
             header={{
@@ -22,12 +26,21 @@ const SingleProjectContent: FunctionComponent<SingleProjectContentProps> = ({ pr
             round="left"
             unlimitedHeight
         >
-            <ProjectDescription description={project.description} amountOfFeatures={project.features.length} />
-
-            <FeaturesList
-                features={project.features} //
-                folder={project.folder}
+            <ProjectDescription
+                description={project.description} //
+                amountOfFeatures={amountOfFeatures}
             />
+
+            {(() => {
+                if (amountOfFeatures > 0) {
+                    return (
+                        <FeaturesList
+                            features={project.features} //
+                            folder={project.folder}
+                        />
+                    );
+                }
+            })()}
         </LightSectionWrapper>
     );
 };
