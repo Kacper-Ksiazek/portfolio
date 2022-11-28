1f84ee6b809603bfdd35fb29c9e323b97c2dccd4
DEVRafam <jodax95@gmail.com>
2022-11-28 03:15:34 +0100

    🐛 FIX: Make it static loaded #80

---
+++ components/pages/landing_page/Wrapper.tsx
@@ -0,0 +1,29 @@
+// Types
+import type { FunctionComponent } from "react";
+import type { LandingPageServerSideProps } from "@/@types/pages/LandingPage";
+// Other components
+import Contact from "@/components/pages/landing_page/Contact";
+import Projects from "@/components/pages/landing_page/Projects";
+import ToDoList from "@/components/pages/landing_page/ToDoList";
+import BreakTheIce from "@/components/pages/landing_page/BreakTheIce";
+import PicturesMatchingGame from "@/components/pages/landing_page/PicturesMatchingGame";
+import ScrollToParticularSection from "@/components/pages/landing_page/ScrollToParticularSection";
+
+const LandingPageWrapper: FunctionComponent<LandingPageServerSideProps> = (props) => {
+    return (
+        <>
+            <ScrollToParticularSection />
+            <BreakTheIce
+                hobbies={props.hobbies} //
+                schools={props.schools}
+                previousJobs={props.previousJobs}
+            />
+            <ToDoList />
+            <Projects projects={props.projects} />
+            <PicturesMatchingGame />
+            <Contact />
+        </>
+    );
+};
+
+export default LandingPageWrapper;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 // Tools
+import dynamic from "next/dynamic";
 import { prisma } from "@/prisma/db";
 import { formatProjectDate } from "@/utils/api/date-formatter";
 // Types
@@ -6,31 +7,16 @@ import type { NextPage, GetServerSideProps } from "next";
 import type { LandingPageServerSideProps } from "@/@types/pages/LandingPage";
 // Other components
 import SEO from "@/components/pages/_SEO";
-import Contact from "@/components/pages/landing_page/Contact";
-import Projects from "@/components/pages/landing_page/Projects";
-import ToDoList from "@/components/pages/landing_page/ToDoList";
-import BreakTheIce from "@/components/pages/landing_page/BreakTheIce";
 import IntroductionScreen from "@/components/pages/landing_page/IntroductionScreen";
-import PicturesMatchingGame from "@/components/pages/landing_page/PicturesMatchingGame";
-import ScrollToParticularSection from "@/components/pages/landing_page/ScrollToParticularSection";
-// Material UI Icons
+const LandingPageContent = dynamic(() => import("@/components/pages/landing_page/Wrapper"));
 
 const Home: NextPage<LandingPageServerSideProps> = (props) => {
     return (
         <>
             <SEO description="Since the age of 16 software engineering enjoyer, then freelancer, now Data Science at AGH in Cracow freshman. This portfolio is an extensive introduction of my person including information regarding my previous education and working experience, my current hobbies and foremost my motives and goals. " />
             <>
-                <ScrollToParticularSection />
                 <IntroductionScreen />
-                <BreakTheIce
-                    hobbies={props.hobbies} //
-                    schools={props.schools}
-                    previousJobs={props.previousJobs}
-                />
-                <ToDoList />
-                <Projects projects={props.projects} />
-                <PicturesMatchingGame />
-                <Contact />
+                <LandingPageContent {...props} />
             </>
         </>
     );
