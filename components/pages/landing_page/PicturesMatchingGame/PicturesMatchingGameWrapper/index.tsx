2fb23e240e17323bd8f1417e42f43425245b178e
DEVRafam <jodax95@gmail.com>
2022-12-22 13:52:06 +0100

    📦 NEW: PicturesMatchingGameWrapper extracted as distinct component

---
+++ components/pages/landing_page/PicturesMatchingGame/PicturesMatchingGameWrapper/index.tsx
@@ -0,0 +1,100 @@
+// Tools
+import { styled } from "@mui/system";
+import { introAnimations } from "./introAnimations";
+// Types
+import type { FunctionComponent, ReactNode } from "react";
+import type { AnimationToDisplay } from "@/@types/pages/PicturesMatchingGame";
+// Material UI Icons
+import SportsEsports from "@mui/icons-material/SportsEsports";
+// Styled components
+import DarkSectionWrapper from "@/components/atoms/content_placement/SectionWrapper/Dark";
+
+const Background = styled("span")(({ theme }) => ({
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    zIndex: 1,
+    opacity: 0.2,
+    "&.INVALID_CHOICE": {
+        background: "#aa1b38",
+    },
+    "&.CORRECT_CHOICE": {
+        background: "#56bc5b",
+    },
+    transition: "background .3s linear",
+}));
+
+interface PicturesMatchingGameWrapperProps {
+    children: ReactNode;
+    animation: AnimationToDisplay;
+}
+
+const PicturesMatchingGameWrapper: FunctionComponent<PicturesMatchingGameWrapperProps> = (props) => {
+    return (
+        <DarkSectionWrapper
+            id="picture-matching-game-main-wrapper"
+            shapesDirection="right"
+            header={{
+                main: "React image matching game",
+                index: 2,
+                icon: <SportsEsports />,
+                description: `Another very frequently seen portfolio project is a images matching game, so I had decided to code my version of it either in order to spice up everything and more importantly to create second content separator.`,
+            }}
+            sx={{
+                "&.visible": {
+                    ...(introAnimations as any),
+                },
+                maxHeight: "540px",
+                transition: "max-height .15s .25s linear",
+                "&.gameplay-on": {
+                    position: "fixed",
+                    top: "-20px",
+                    left: 0,
+                    width: "100vw",
+                    height: "100vh",
+                    maxHeight: "100vh",
+                    borderRadius: "0",
+                    overflowY: "scroll",
+                    zIndex: 10000,
+                    transition: "max-height .15s linear",
+                    ".dark-section-wrapper-background-svg, #user-choice-animaiton-base": {
+                        position: "fixed",
+                        width: "calc(100vw - 8px)",
+                        height: "100vh",
+                        top: 0,
+                        left: 0,
+                    },
+                    ".dark-content-wrapper-github-redirection": {
+                        display: "none",
+                    },
+                    "&::-webkit-scrollbar": {
+                        width: "8px",
+                        background: "#f0eff4",
+                    },
+
+                    "&::-webkit-scrollbar-track": {
+                        boxShadow: "inset 0 0 2px #888",
+                        opacity: 0.1,
+                    },
+                    "&::-webkit-scrollbar-thumb": {
+                        backgroundColor: "#da4167",
+                        borderRadius: "2px",
+                    },
+                },
+            }}
+            githubURL="https://github.com/Kacper-Ksiazek/portfolio/tree/main/components/pages/landing_page/PicturesMatchingGame"
+            childrenOutsideContent={
+                <Background
+                    id="user-choice-animaiton-base" //
+                    className={props.animation ?? ""}
+                />
+            }
+        >
+            {props.children}
+        </DarkSectionWrapper>
+    );
+};
+
+export default PicturesMatchingGameWrapper;
--- components/pages/landing_page/PicturesMatchingGame/index.tsx
+++ components/pages/landing_page/PicturesMatchingGame/index.tsx
@@ -1,6 +1,4 @@
 // Tools
-import { styled } from "@mui/system";
-import { introAnimations } from "./introAnimations";
 import { useLazyLoadedImages } from "@/hooks/useLazyLoadedImages";
 import ALL_AVAILABLE_IMAGES from "@/data/pictures_for_matching_game";
 import { usePicturesMatchingGameContext } from "./hooks/usePicturesMatchingGameContext";
@@ -11,27 +9,9 @@ import Summary from "./Summary";
 import PickADifficulty from "./PickADifficulty";
 import ImageModel from "@/components/utils/ImageModel";
 import { PicturesMatchingGameContextProvider } from "./context";
-// Material UI Icons
-import SportsEsports from "@mui/icons-material/SportsEsports";
 // Styled Components
 import Gameplay from "./Gameplay";
-import DarkSectionWrapper from "@/components/atoms/content_placement/SectionWrapper/Dark";
-const Background = styled("span")(({ theme }) => ({
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    zIndex: 1,
-    opacity: 0.2,
-    "&.INVALID_CHOICE": {
-        background: "#aa1b38",
-    },
-    "&.CORRECT_CHOICE": {
-        background: "#56bc5b",
-    },
-    transition: "background .3s linear",
-}));
+import PicturesMatchingGameWrapper from "./PicturesMatchingGameWrapper";
 
 const PicturesMatchingGame: FunctionComponent = (props) => {
     const context = usePicturesMatchingGameContext();
@@ -47,65 +27,7 @@ const PicturesMatchingGame: FunctionComponent = (props) => {
     });
 
     return (
-        <DarkSectionWrapper
-            id="picture-matching-game-main-wrapper"
-            shapesDirection="right"
-            header={{
-                main: "React image matching game",
-                index: 2,
-                icon: <SportsEsports />,
-                description: `Another very frequently seen portfolio project is a images matching game, so I had decided to code my version of it either in order to spice up everything and more importantly to create second content separator.`,
-            }}
-            sx={{
-                "&.visible": {
-                    ...(introAnimations as any),
-                },
-                maxHeight: "540px",
-                transition: "max-height .15s .25s linear",
-                "&.gameplay-on": {
-                    position: "fixed",
-                    top: "-20px",
-                    left: 0,
-                    width: "100vw",
-                    height: "100vh",
-                    maxHeight: "100vh",
-                    borderRadius: "0",
-                    overflowY: "scroll",
-                    zIndex: 10000,
-                    transition: "max-height .15s linear",
-                    ".dark-section-wrapper-background-svg, #user-choice-animaiton-base": {
-                        position: "fixed",
-                        width: "calc(100vw - 8px)",
-                        height: "100vh",
-                        top: 0,
-                        left: 0,
-                    },
-                    ".dark-content-wrapper-github-redirection": {
-                        display: "none",
-                    },
-                    "&::-webkit-scrollbar": {
-                        width: "8px",
-                        background: "#f0eff4",
-                    },
-
-                    "&::-webkit-scrollbar-track": {
-                        boxShadow: "inset 0 0 2px #888",
-                        opacity: 0.1,
-                    },
-                    "&::-webkit-scrollbar-thumb": {
-                        backgroundColor: "#da4167",
-                        borderRadius: "2px",
-                    },
-                },
-            }}
-            githubURL="https://github.com/Kacper-Ksiazek/portfolio/tree/main/components/pages/landing_page/PicturesMatchingGame"
-            childrenOutsideContent={
-                <Background
-                    id="user-choice-animaiton-base" //
-                    className={context.gameplay.animation ?? ""}
-                />
-            }
-        >
+        <PicturesMatchingGameWrapper animation={context.gameplay.animation}>
             {(() => {
                 if (context.pictureToDisplayInFullsize) {
                     return (
@@ -127,11 +49,10 @@ const PicturesMatchingGame: FunctionComponent = (props) => {
                         return <Gameplay />;
                     case "SUMMARY":
                         return <Summary />;
+                        return <></>;
                 }
-
-                return <></>;
             })()}
-        </DarkSectionWrapper>
+        </PicturesMatchingGameWrapper>
     );
 };
 
