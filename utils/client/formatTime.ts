90af59d2400ee2ee57c28beb1435517f12f71de0
DEVRafam <jodax95@gmail.com>
2022-12-29 06:30:10 +0100

    👌 IMPROVE: formatTime became more versatile

--- utils/client/formatTime.ts
+++ utils/client/formatTime.ts
@@ -1,7 +1,75 @@
-const displayAugmentedDigit = (digit: number | string): number | string => {
-    return String(digit).length === 1 ? `0${digit}` : digit;
+type OutputType = "CLOCK" | "SHORT" | "LONG";
+
+interface MinSecTimeFormat {
+    minutes: number;
+    seconds: number;
+}
+
+interface SecOnlyTimeFormat {
+    seconds: number;
+}
+
+interface FormatTimeParams {
+    time: MinSecTimeFormat | SecOnlyTimeFormat;
+    outputType: OutputType;
+}
+
+export const formatTime = (params: FormatTimeParams): string => {
+    let [minutes, seconds] = [0, 0];
+
+    const timeIsSecOnlyType = isSecOnlyTimeFormat(params.time);
+    const timeIsMinSecTimeFormat = isMinSecTimeFormat(params.time);
+
+    if (!timeIsSecOnlyType && !timeIsMinSecTimeFormat) return "-";
+
+    if (timeIsMinSecTimeFormat) {
+        minutes = (params.time as MinSecTimeFormat).minutes;
+        seconds = params.time.seconds;
+    } else {
+        minutes = Math.floor(params.time.seconds / 60);
+        seconds = params.time.seconds % 60;
+    }
+
+    switch (params.outputType) {
+        case "CLOCK":
+            return `${displayAugmentedDigit(minutes)}:${displayAugmentedDigit(seconds)}`;
+        case "SHORT":
+            if (minutes > 0) return `${minutes} min ${seconds} sec`;
+            return `${seconds} seconds`;
+        case "LONG":
+            if (minutes > 0) return `${minutes} min ${seconds} sec`;
+            return `${seconds} seconds`;
+    }
 };
 
-export const formatTime = ({ minutes, seconds }: { minutes: number; seconds: number }): string => {
-    return `${displayAugmentedDigit(minutes)}:${displayAugmentedDigit(seconds)}`;
+const isMinSecTimeFormat = (params: unknown): params is MinSecTimeFormat => {
+    if (params instanceof Object) {
+        const keys = Object.keys(params);
+
+        return (
+            keys.length === 2 && //
+            keys.includes("minutes" as keyof MinSecTimeFormat) &&
+            keys.includes("seconds" as keyof MinSecTimeFormat) &&
+            !isNaN(Number((params as Record<keyof MinSecTimeFormat, unknown>)["minutes"])) &&
+            !isNaN(Number((params as Record<keyof MinSecTimeFormat, unknown>)["seconds"]))
+        );
+    }
+    return false;
+};
+
+const isSecOnlyTimeFormat = (params: unknown): params is SecOnlyTimeFormat => {
+    if (params instanceof Object) {
+        const keys = Object.keys(params);
+
+        return (
+            keys.length === 1 && //
+            keys.includes("seconds" as keyof MinSecTimeFormat) &&
+            !isNaN(Number((params as Record<keyof MinSecTimeFormat, unknown>)["seconds"]))
+        );
+    }
+    return false;
+};
+
+const displayAugmentedDigit = (digit: number | string): number | string => {
+    return String(digit).length === 1 ? `0${digit}` : digit;
 };
