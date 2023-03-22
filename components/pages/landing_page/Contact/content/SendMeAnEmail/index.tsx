ea9c1cf2d5e7968074603e432ec8a456c4bdcd28
DEVRafam <jodax95@gmail.com>
2023-03-22 02:19:02 +0100

    👌 IMPROVE: Landing Page / Contact / Lazy loading for map images
    See #86

--- components/pages/landing_page/Contact/content/SendMeAnEmail/index.tsx
+++ components/pages/landing_page/Contact/content/SendMeAnEmail/index.tsx
@@ -1,10 +1,6 @@
 // Tools
-import { useEffect } from "react";
 import { useLocalStorage } from "@/hooks/useLocalStorage";
-import { useRequestContext } from "./hooks/useRequestContext";
-import { useSendRequestQuery } from "./hooks/queries/useSendRequestQuery";
-import { useSpecialWayOfRendering } from "./hooks/useSpecialWayOfRendering";
-import { useMapContext } from "@/components/pages/landing_page/Contact/hooks/useMapContext";
+import { useLazyLoadedMapImages, useMapStatusUpdater, useRequestContext, useSendRequestQuery, useSpecialWayOfRendering } from "./hooks";
 // Types
 import type { FunctionComponent } from "react";
 // Other components
@@ -15,25 +11,14 @@ import { RequestContextProvider } from "./contexts/requestContext";
 import SectionWrapper from "../_SectionWrapper";
 
 const SendMeAnEmail: FunctionComponent = () => {
+    const { request } = useRequestContext();
     const [alreadySentEmail, setAlreadySentEmail] = useLocalStorage<string | null>("email-has-been-already-send", null, { keepOriginalValue: true });
 
     const specialWayOfRendering = useSpecialWayOfRendering();
     const sendRequest = useSendRequestQuery(setAlreadySentEmail);
 
-    const { request } = useRequestContext();
-    const { changeMapStatus } = useMapContext();
-
-    useEffect(() => {
-        const status = request.status;
-
-        if (status === "error" || status === "staged_error") {
-            changeMapStatus("error");
-        } //
-        else if (status === "success" || status === "staged_success" || alreadySentEmail) {
-            changeMapStatus("success");
-        } //
-        else changeMapStatus(null);
-    }, [request.status, changeMapStatus, alreadySentEmail]);
+    useLazyLoadedMapImages();
+    useMapStatusUpdater(request.status, alreadySentEmail);
 
     return (
         <SectionWrapper id="send-me-en-email-wrapper" sx={{ height: "460px" }}>
