33e97f053efa206a69d14e698f9570523d9b5d67
DEVRafam <jodax95@gmail.com>
2023-04-22 18:16:14 +0200

    🐛 FIX: ToDoList / Unexisting import resolved See #88

--- components/pages/landing_page/ToDoList2/ProgressTracker/LabelsRatio/ThereAreNoRatios.tsx
+++ components/pages/landing_page/ToDoList2/ProgressTracker/LabelsRatio/ThereAreNoRatios.tsx
@@ -1,13 +1,13 @@
 // Types
 import type { FunctionComponent } from "react";
 // Styled components
-import { SingleBar, SingleBarWrapper } from "../styled_components";
+import { ProgressBar, FlexWrapper } from "../styled_components";
 
 const ThereAreNoRatios: FunctionComponent = () => {
     return (
-        <SingleBarWrapper>
-            <SingleBar completion="0%" color="#333" />
-        </SingleBarWrapper>
+        <FlexWrapper>
+            <ProgressBar completion="0%" color="#333" />
+        </FlexWrapper>
     );
 };
 
