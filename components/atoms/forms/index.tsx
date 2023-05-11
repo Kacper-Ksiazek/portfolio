74a56710c80d70f07a2d3ddba2813cb3df87154f
DEVRafam <jodax95@gmail.com>
2023-05-11 22:54:15 +0200

    📦 NEW: atoms / StyledColorPicker See #88

---
+++ components/atoms/forms/index.tsx
@@ -0,0 +1,7 @@
+import StyledInput from "./StyledInput";
+import StyledButton from "./StyledButton";
+import StyledSelect from "./StyledSelect";
+import ButtonWithTooltip from "./ButtonWithTooltip";
+import StyledColorPicker from "./StyledColorPicker";
+
+export { StyledButton, StyledColorPicker, StyledInput, StyledSelect, ButtonWithTooltip };
