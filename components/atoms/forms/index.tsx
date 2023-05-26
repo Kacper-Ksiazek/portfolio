a1dac204b5d9ea7a89347a488d1177b190d50a38
DEVRafam <jodax95@gmail.com>
2023-05-27 01:05:07 +0200

    👌 IMPROVE: StyledCheckbox added to index exports organizer See #88

--- components/atoms/forms/index.tsx
+++ components/atoms/forms/index.tsx
@@ -1,7 +1,8 @@
 import StyledInput from "./StyledInput";
 import StyledButton from "./StyledButton";
 import StyledSelect from "./StyledSelect";
+import StyledCheckbox from "./StyledCheckbox";
 import ButtonWithTooltip from "./ButtonWithTooltip";
 import StyledColorPicker from "./StyledColorPicker";
 
-export { StyledButton, StyledColorPicker, StyledInput, StyledSelect, ButtonWithTooltip };
+export { StyledButton, StyledColorPicker, StyledInput, StyledSelect, ButtonWithTooltip, StyledCheckbox };
