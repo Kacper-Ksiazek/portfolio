9fb7cae8483e69f10ad448b1dcb038362e5c4428
Kacper-Ksiazek <jodax95@gmail.com>
2023-06-12 11:45:56 +0200

    🐛 FIX: ConfirmationModal  / reopening issue resolved See #88

--- components/pages/landing_page/ToDoList2/ActionsHeader/Content/EditLabels/ManagementHeader/DeleteUnusedLabels.tsx
+++ components/pages/landing_page/ToDoList2/ActionsHeader/Content/EditLabels/ManagementHeader/DeleteUnusedLabels.tsx
@@ -1,10 +1,12 @@
 // Tools
-import { useMemo } from "react";
+import { useMemo, useRef } from "react";
 // Types
 import type { FunctionComponent } from "react";
 import type { LabelID, TaskCountsCollection } from "landing_page/ToDoList2/@types";
 // Other components
+import Label from "landing_page/ToDoList2/atoms/LabelBase";
 import StyledButton from "@/components/atoms/forms/StyledButton";
+import ConfirmationModal from "@/components/atoms/ConfirmationModal";
 // Material UI Icons
 import CleaningServicesRounded from "@mui/icons-material/CleaningServicesRounded";
 
@@ -13,6 +15,8 @@ interface DeleteUnusedLabelsProps {
 }
 
 const DeleteUnusedLabels: FunctionComponent<DeleteUnusedLabelsProps> = (props) => {
+    const modalOpeningButtonRef = useRef<HTMLButtonElement | null>(null);
+
     const unusedLabels: LabelID[] = useMemo<LabelID[]>(() => {
         return [...props.counter.entries()]
             .filter(([_, counts]) => {
@@ -26,12 +30,36 @@ const DeleteUnusedLabels: FunctionComponent<DeleteUnusedLabelsProps> = (props) =
     function onClick() {
         if (unusedLabels.length === 0) return;
     }
-
+    // jest problem z otwieraniem dwukrotnie trgo amego confirmation modala naprawic to !!! MILEGO DNIA SAMEMU SOBIE
     return (
-        <StyledButton color="MUIFormElement" subtleHoverEffect>
-            <CleaningServicesRounded sx={{ mr: "6px" }} />
-            Delete {unusedLabels.length} unused labels
-        </StyledButton>
+        <>
+            <StyledButton
+                color="MUIFormElement" //
+                onClick={onClick}
+                ref={modalOpeningButtonRef}
+            >
+                <CleaningServicesRounded sx={{ mr: "6px" }} />
+                Delete {unusedLabels.length} unused labels
+            </StyledButton>
+
+            <ConfirmationModal
+                ref={modalOpeningButtonRef}
+                title="Delete unused labels"
+                actionButton={{
+                    disabled: false,
+                    onClick: () => {},
+                    prompt: "Remove",
+                }}
+            >
+                <>
+                    <p>
+                        Are you sure you want to remove <strong>{unusedLabels.length}</strong> labels which are currently not in use?
+                    </p>
+
+                    {unusedLabels.join(" | ")}
+                </>
+            </ConfirmationModal>
+        </>
     );
 };
 
