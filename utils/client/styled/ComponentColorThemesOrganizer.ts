342cbae62db4cc209430d20f07139c0c8887c504
Kacper-Ksiazek <jodax95@gmail.com>
2023-07-24 23:50:59 +0200

    📦 NEW: utils / styled / ComponentColorThemesOrganizer See #88

---
+++ utils/client/styled/ComponentColorThemesOrganizer.ts
@@ -0,0 +1,39 @@
+// Tools
+import { alpha } from "@mui/material";
+// Types
+import type { Theme } from "@mui/material";
+
+interface ComponentColorTheme {
+    fontColor: string;
+    background: string;
+
+    borderColor?: string;
+}
+
+interface ComponentColorThemeWithFixedBorder {
+    fontColor: string;
+    background: string;
+    borderColor: string;
+}
+
+type AvailableThemes<T extends string> = Record<T, AvailableThemesObjectProperty>;
+type AvailableThemesObjectProperty = ComponentColorTheme | ((theme: Theme) => ComponentColorTheme);
+
+export class ComponentColorThemesOrganizer<T extends string> {
+    public constructor(public availableThemes: AvailableThemes<T>) {}
+
+    public getTheme(componentThemeID: T, MUITheme: Theme): ComponentColorThemeWithFixedBorder {
+        const chosenTheme = this.availableThemes[componentThemeID];
+        const { background, fontColor, borderColor }: ComponentColorTheme = typeof chosenTheme === "function" ? chosenTheme(MUITheme) : chosenTheme;
+
+        return {
+            fontColor: fontColor,
+            background: background,
+            borderColor: borderColor ?? this.createDefaultBorderColor(fontColor),
+        };
+    }
+
+    private createDefaultBorderColor(fontColor: string): string {
+        return alpha(fontColor, 0.23);
+    }
+}
