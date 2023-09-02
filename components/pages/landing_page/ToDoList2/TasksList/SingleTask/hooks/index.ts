9fe0190eab4d16d85f1aff14d9810661cc8559f6
Kacper-Ksiazek <jodax95@gmail.com>
2023-09-02 14:25:48 +0200

    🐛 FIX: useUpdatingTaskValidator removed as redundant piece of code See #88

--- components/pages/landing_page/ToDoList2/TasksList/SingleTask/hooks/index.ts
+++ components/pages/landing_page/ToDoList2/TasksList/SingleTask/hooks/index.ts
@@ -1,6 +1,5 @@
 import { useTaskRemover } from "./useTaskRemover";
 import { useEditModeContext } from "./useEditModeContext";
-import { useUpdatingTaskValidator } from "./useUpdatingTaskValidator";
 import { useValidationResultContext } from "./useValidationResultContext";
 
-export { useTaskRemover, useEditModeContext, useUpdatingTaskValidator, useValidationResultContext };
+export { useTaskRemover, useEditModeContext, useValidationResultContext };
