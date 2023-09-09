dafbaa421af10436653d6e0ffe92fdb6569173a1
Kacper-Ksiazek <jodax95@gmail.com>
2023-09-09 09:38:15 +0200

    📦 NEW: Layout is not a statealone component See #88

---
+++ components/pages/landing_page/ToDoList/Layout/index.tsx
@@ -0,0 +1,53 @@
+// Tools
+import { forwardRef } from "react";
+// Types
+import type { Release } from "./@types";
+import type { ReactNode } from "react";
+// Material UI Icons
+import Code from "@mui/icons-material/Code";
+// Material UI Components
+import Box from "@mui/material/Box";
+// Other components
+import * as HeaderElements from "./HeaderElements";
+import DarkSectionWrapper from "@/components/atoms/content_placement/SectionWrapper/Dark";
+
+interface ToDoListLayoutProps {
+    currentRelease: Release;
+    releaseIsChanging: boolean;
+    children: ReactNode;
+
+    toggleReleases: () => void;
+}
+
+const ToDoListLayout = forwardRef<HTMLDivElement, ToDoListLayoutProps>((props, ref) => {
+    const { currentRelease, releaseIsChanging, toggleReleases } = props;
+
+    return (
+        <DarkSectionWrapper
+            ref={ref}
+            shapesDirection="left"
+            header={{
+                main: <HeaderElements.Title currentRelease={currentRelease} isChanging={releaseIsChanging} />,
+                index: 1,
+                icon: <Code />,
+                description: <HeaderElements.Description currentRelease={currentRelease} isChanging={releaseIsChanging} />,
+            }}
+            githubURL={"https://github.com/Kacper-Ksiazek/portfolio/tree/main/components/pages/landing_page"}
+        >
+            <Box
+                sx={{
+                    opacity: releaseIsChanging ? 0 : 1,
+                    transition: "opacity 0.3s ease-in-out",
+                }}
+            >
+                {props.children}
+            </Box>
+
+            <button onClick={toggleReleases}>mvp</button>
+        </DarkSectionWrapper>
+    );
+});
+
+ToDoListLayout.displayName = "ToDoListLayout";
+
+export default ToDoListLayout;
--- components/pages/landing_page/ToDoList/index.tsx
+++ components/pages/landing_page/ToDoList/index.tsx
@@ -1,17 +1,12 @@
 // Tools
 import { useRef } from "react";
 import dynamic from "next/dynamic";
-import { useReleases } from "./hooks/useReleases";
+import { useReleases } from "./Layout/hooks/useReleases";
 // Types
 import type { FunctionComponent } from "react";
-// Material UI Icons
-import Code from "@mui/icons-material/Code";
-// Material UI Components
-import Box from "@mui/material/Box";
 // Other components
-import * as HeaderElements from "./HeaderElements";
 import Release2023 from "./2023";
-import DarkSectionWrapper from "@/components/atoms/content_placement/SectionWrapper/Dark";
+import ToDoListLayout from "./Layout";
 
 const Legacy = dynamic(() => import("./Legacy"));
 
@@ -21,28 +16,14 @@ const ToDoList: FunctionComponent = () => {
     const { currentRelease, releaseIsChanging, toggleReleases } = useReleases(mainWrapperRef);
 
     return (
-        <DarkSectionWrapper
-            ref={mainWrapperRef}
-            shapesDirection="left"
-            header={{
-                main: <HeaderElements.Title currentRelease={currentRelease} isChanging={releaseIsChanging} />,
-                index: 1,
-                icon: <Code />,
-                description: <HeaderElements.Description currentRelease={currentRelease} isChanging={releaseIsChanging} />,
-            }}
-            githubURL={"https://github.com/Kacper-Ksiazek/portfolio/tree/main/components/pages/landing_page"}
+        <ToDoListLayout
+            ref={mainWrapperRef} //
+            currentRelease={currentRelease}
+            releaseIsChanging={releaseIsChanging}
+            toggleReleases={toggleReleases}
         >
-            <Box
-                sx={{
-                    opacity: releaseIsChanging ? 0 : 1,
-                    transition: "opacity 0.3s ease-in-out",
-                }}
-            >
-                {currentRelease === "2023" ? <Release2023 /> : <Legacy />}
-            </Box>
-
-            <button onClick={toggleReleases}>mvp</button>
-        </DarkSectionWrapper>
+            {currentRelease === "2023" ? <Release2023 /> : <Legacy />}
+        </ToDoListLayout>
     );
 };
 
