7278b552165200cc5f60c16970bda2e12ed3618e
Kacper-Ksiazek <jodax95@gmail.com>
2023-09-25 21:45:00 +0200

    📦 NEW: New favicon See #113

--- components/pages/_SEO.tsx
+++ components/pages/_SEO.tsx
@@ -43,7 +43,14 @@ const SEO: FunctionComponent<SEOProps> = (props) => {
             <meta key="twitter:title" property="twitter:title" content={title} />
             <meta key="twitter:description" property="twitter:description" content={props.description} />
 
-            <link rel="shortcut icon" href="/favicon.ico" />
+            {/* Favicon */}
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+            <link rel="manifest" href="/favicon/site.webmanifest" />
+            <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#832161" />
+            <meta name="msapplication-TileColor" content="#F9FAFB" />
+            <meta name="theme-color" content="#161619" />
         </Head>
     );
 };
