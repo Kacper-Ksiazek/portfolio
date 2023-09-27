7a0162146d969dcd082b9dccca09a972a738f479
Kacper-Ksiazek <jodax95@gmail.com>
2023-09-27 18:39:35 +0200

    🐛 FIX: Light theme for logo added See #114

--- layout/Footer/Content/FooterLogoAnimation.tsx
+++ layout/Footer/Content/FooterLogoAnimation.tsx
@@ -12,51 +12,56 @@ import { fadeSimpleOUT } from "@/components/keyframes/outro/fade";
 // Styled components
 const AnimationStep = styled("span", {
     shouldForwardProp: (prop) => prop !== "step",
-})<{ step: number }>(({ theme, ...props }) => ({
-    ...theme.mixins.absolute_full,
-    backgroundPosition: "center !important",
-    background: `url(/main-page-logo/animations/step_${props.step}.png)`,
+})<{ step: number }>(({ theme, ...props }) => {
+    const folderName: "dark_theme" | "light_theme" = theme.palette.mode === "light" ? "light_theme" : "dark_theme";
+    const imgName: `${string}.png` = `step_${props.step}.png`;
 
-    "&.step-1, &.step-2, &.step-3, &.step-6, &.step-5": {
-        zIndex: 1,
-    },
+    return {
+        ...theme.mixins.absolute_full,
+        backgroundPosition: "center !important",
+        background: `url(/main-page-logo/animations/${folderName}/${imgName})`,
 
-    opacity: 0,
+        "&.step-1, &.step-2, &.step-3, &.step-6, &.step-5": {
+            zIndex: 1,
+        },
 
-    "&.step-1": {
-        animation: `${fadeSimple} .24s .3s both`,
-    },
-    "&.step-2": {
-        animation: `${fadeSimple} .24s .5s both`,
-    },
-    "&.step-3": {
-        animation: `${fadeSimple} .24s .7s both`,
-    },
-    "&.step-4": {
-        animation: `${fadeFromRight} .3s 1.1s both`,
-    },
-    "&.step-5": {
-        animation: `${fadeFromLeft} .3s 1.1s both`,
-    },
-    "&.step-6": {
-        animation: `${fadeFromTop} 1s .9s both`,
-    },
-    "&.step-7": {
-        animation: `${fadeFromBottom} 1s 1.2s both`,
-    },
-    "&.step-8": {
-        animation: `${fadeFromRight} 1s 1.24s both`,
-    },
-    "&.step-9": {
-        animation: `${fadeFromLeft} 1s 1.24s both`,
-    },
-    "&.step-10": {
-        animation: `${fadeFromRight} .2s .9s both`,
-    },
-    "&.step-11": {
-        animation: `${fadeFromLeft} .2s .9s both`,
-    },
-}));
+        opacity: 0,
+
+        "&.step-1": {
+            animation: `${fadeSimple} .24s .3s both`,
+        },
+        "&.step-2": {
+            animation: `${fadeSimple} .24s .5s both`,
+        },
+        "&.step-3": {
+            animation: `${fadeSimple} .24s .7s both`,
+        },
+        "&.step-4": {
+            animation: `${fadeFromRight} .3s 1.1s both`,
+        },
+        "&.step-5": {
+            animation: `${fadeFromLeft} .3s 1.1s both`,
+        },
+        "&.step-6": {
+            animation: `${fadeFromTop} 1s .9s both`,
+        },
+        "&.step-7": {
+            animation: `${fadeFromBottom} 1s 1.2s both`,
+        },
+        "&.step-8": {
+            animation: `${fadeFromRight} 1s 1.24s both`,
+        },
+        "&.step-9": {
+            animation: `${fadeFromLeft} 1s 1.24s both`,
+        },
+        "&.step-10": {
+            animation: `${fadeFromRight} .2s .9s both`,
+        },
+        "&.step-11": {
+            animation: `${fadeFromLeft} .2s .9s both`,
+        },
+    };
+});
 
 const FooterLogoAnimation: FunctionComponent = () => {
     const pathname: string = window ? window.location.pathname : "loading";
