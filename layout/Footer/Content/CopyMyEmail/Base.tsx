85b58e42358b7ec0924c64a985b97d6df60bfe27
Kacper-Ksiazek <jodax95@gmail.com>
2023-09-28 22:02:59 +0200

    📦 NEW: Vague logo in background See #114

--- layout/Footer/Content/CopyMyEmail/Base.tsx
+++ layout/Footer/Content/CopyMyEmail/Base.tsx
@@ -1,8 +1,22 @@
 // Tools
 import { styled } from "@mui/material";
+
 // Styled components
 export default styled("div")(({ theme }) => ({
     ...theme.mixins.flex_center,
     flexDirection: "column",
     height: "100%",
+    "&::before": {
+        content: "''",
+        position: "absolute",
+        ...theme.mixins.absolute_full,
+        background: `url(./main-page-logo/grayscale/${theme.palette.mode}.png)`,
+        backgroundPosition: "center",
+        opacity: 0.2,
+        zIndex: 0,
+    },
+    "&>*": {
+        position: "relative",
+        zIndex: 1,
+    },
 }));
