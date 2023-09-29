d6b5190efe8698b7c29de1eb054322074c0eae78
Kacper-Ksiazek <jodax95@gmail.com>
2023-09-29 23:41:51 +0200

    📦 NEW: Change back to logo on every route change See #114

--- layout/Footer/context/Provider.tsx
+++ layout/Footer/context/Provider.tsx
@@ -1,6 +1,7 @@
 // Tools
-import { useState } from "react";
 import { footerContext } from ".";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 // Types
 import type { FooterContent } from ".";
 import type { FunctionComponent, ReactNode } from "react";
@@ -11,6 +12,12 @@ interface FooterContextProviderProps {
 
 const FooterContextProvider: FunctionComponent<FooterContextProviderProps> = (props) => {
     const [contentToDisplay, setContentToDisplay] = useState<FooterContent>("LOGO");
+    const router = useRouter();
+
+    // Set content to display on route change
+    useEffect(() => {
+        setContentToDisplay("LOGO");
+    }, [router.pathname]);
 
     return (
         <footerContext.Provider
