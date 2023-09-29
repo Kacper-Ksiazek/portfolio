9bfaa5d0e89891d82e9aa621c6552509998bc195
Kacper-Ksiazek <jodax95@gmail.com>
2023-09-29 10:46:17 +0200

    📦 NEW: atoms / NavigationBetweenSections - each step button now can be assigned all html attributes See #114

--- components/atoms/NavigationBetweenSections/index.tsx
+++ components/atoms/NavigationBetweenSections/index.tsx
@@ -3,8 +3,8 @@ import { CSS_REFERENCES } from "./css_references";
 import { parseSection } from "./utils/parseSection";
 // Types
 import type { ReactNode } from "react";
-import type { UnparsedSectionElement } from "./@types";
 import type { Styles } from "@/@types/MUI";
+import type { UnparsedSectionElement, SectionElement } from "./@types";
 // Styled components
 import { Divider, SingleNavigationStep, NavigationBetweenSectionsBase } from "./styled_components";
 
@@ -17,6 +17,13 @@ interface NavigationBetweenSectionsProps<T> {
     leftSideChildren?: ReactNode;
     rightSideChildren?: ReactNode;
     beforeOnClick?: () => Promise<void> | void;
+}
+
+function getStepButtonCSSClassName(props: SectionElement<any>["props"]): string {
+    // If the props are not defined or the className is not given in the props, return only the default class
+    if (!props || "className" in props === false) return CSS_REFERENCES.STEP_BUTTON;
+
+    return `${CSS_REFERENCES.STEP_WRAPPER} ${props.className}`;
 }
 
 const NavigationBetweenSections = <T extends string>(props: NavigationBetweenSectionsProps<T>) => {
@@ -37,7 +44,7 @@ const NavigationBetweenSections = <T extends string>(props: NavigationBetweenSec
             )}
 
             {props.sections.map((item, index) => {
-                const { label, value, disabled } = parseSection<T>(item);
+                const { label, value, disabled, props: propsToForwardForThisParticularItem } = parseSection<T>(item);
 
                 function onClick() {
                     if (disabled !== true) handleOnClick(value);
@@ -48,7 +55,8 @@ const NavigationBetweenSections = <T extends string>(props: NavigationBetweenSec
                         {index ? <Divider className={CSS_REFERENCES.DIVIDER} /> : <span />}
 
                         <SingleNavigationStep
-                            className={CSS_REFERENCES.STEP_BUTTON} //
+                            {...propsToForwardForThisParticularItem}
+                            className={getStepButtonCSSClassName(propsToForwardForThisParticularItem)} //
                             selected={props.currentSection === value}
                             disabled={disabled}
                             //
