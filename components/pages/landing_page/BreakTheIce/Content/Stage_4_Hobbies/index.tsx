46ccad009932130efcb487dd339fdc4846487b5f
Kacper-Ksiazek <jodax95@gmail.com>
2023-10-14 21:33:28 +0200

    🐛 FIX: Problem with changed amount of hobbies causing carousel to glitch See #90

--- components/pages/landing_page/BreakTheIce/Content/Stage_4_Hobbies/index.tsx
+++ components/pages/landing_page/BreakTheIce/Content/Stage_4_Hobbies/index.tsx
@@ -25,7 +25,7 @@ const Hobbies: FunctionComponent = () => {
                 )}
             </Paragraph>
             <Carousel
-                itemsInTotal={3} //
+                itemsInTotal={4} //
                 itemsPerSlide={width <= 550 ? 1 : 2}
                 spacing={40}
                 wrapperSx={{ mt: "20px", ...(RWD as any) }}
