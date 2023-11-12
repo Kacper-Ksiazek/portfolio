445ebeb9e7582d715da64b98f6cacdc7dfad20be
Kacper-Ksiazek <jodax95@gmail.com>
2023-11-12 02:22:59 +0100

    🐛 FIX: missing constants.ts file See #125

---
+++ components/pages/landing_page/IntroductionScreen/Base/hooks/useIntroAnimationControls/constatns.ts
@@ -0,0 +1,5 @@
+export const TIME_TO_UNLOCK_SCROLL: TimeInMS = 2800;
+export const LINES_INTRO_ANIMATION_DURATION: TimeInMS = 4000;
+
+/** Introduction Screen Screen Background Shapes Animation */
+export const URL_QUERY_NAME: string = "skip-introduction-screen-rectangle-animations";
