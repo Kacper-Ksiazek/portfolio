6aeadf040f925c92f47dedd6bb3835c275a6c895
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-03-08 14:12:56 +0100

    Fix problem with missing import SocialMediaRedirection solved See #181

--- components/pages/cv/index.tsx
+++ components/pages/cv/index.tsx
@@ -1,4 +1,5 @@
 import PickLanguage from "./PickLanguage";
 import PickColorTheme from "./PickColorTheme";
+import SocialMediaRedirections from "./SocialMediaRedirections";
 
-export { PickLanguage, PickColorTheme };
+export { PickLanguage, PickColorTheme, SocialMediaRedirections };
